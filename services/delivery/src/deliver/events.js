52b7bcab93e78ea90e2075b30f0f68077e45cba3
Jacob Bare <jacob@parameter1.com>
2022-12-13 12:38:18 -0600

    Upsert aggregated events when delivered

--- services/delivery/src/deliver/events.js
+++ services/delivery/src/deliver/events.js
@@ -1,4 +1,5 @@
 const db = require('../db');
+const dayjs = require('../dayjs');
 
 const getAdIds = async (adId) => {
   const ad = await db.findById('ads', adId, { projection: { lineitemId: 1, advertiserId: 1 } });
@@ -11,6 +12,40 @@ const getAdIds = async (adId) => {
   };
 };
 
+const aggregateEvent = async ({
+  action,
+
+  adId,
+  lineitemId,
+  orderId,
+  advertiserId,
+
+  adunitId,
+  deploymentId,
+  publisherId,
+
+  now,
+}) => {
+  const day = dayjs.tz(now, 'America/Chicago').format('YYYY-MM-DD');
+  const _id = {
+    ad: adId,
+    adunit: adunitId,
+    day,
+  };
+
+  return db.collection('events/aggregated').updateOne({ _id }, {
+    $setOnInsert: { _id },
+    $set: {
+      lineitemId,
+      orderId,
+      advertiserId,
+      deploymentId,
+      publisherId,
+    },
+    $inc: { [`actions.${action}`]: 1 },
+  }, { upsert: true });
+};
+
 module.exports = {
   async view(adunit, correlator, adId, {
     now,
@@ -21,19 +56,32 @@ module.exports = {
   }) {
     const { _id: adunitId, deploymentId, publisherId } = adunit;
     const ids = await getAdIds(adId);
-    return db.insertOne('events', {
-      ...ids,
-      type: 'view',
-      adunitId,
-      deploymentId,
-      publisherId,
-      date: now,
-      email,
-      send,
-      correlator,
-      ip,
-      ua,
-    });
+
+    return Promise.all([
+      // aggregated
+      aggregateEvent({
+        action: 'view',
+        ...ids,
+        adunitId,
+        deploymentId,
+        publisherId,
+        now,
+      }),
+
+      db.insertOne('events', {
+        ...ids,
+        type: 'view',
+        adunitId,
+        deploymentId,
+        publisherId,
+        date: now,
+        email,
+        send,
+        correlator,
+        ip,
+        ua,
+      }),
+    ]);
   },
 
   async click(adunit, correlator, adId, {
@@ -49,18 +97,31 @@ module.exports = {
     if (!view) await this.view(adunit, correlator, adId, { now, email, send });
 
     const ids = await getAdIds(adId);
-    return db.insertOne('events', {
-      ...ids,
-      type: 'click',
-      adunitId,
-      deploymentId,
-      publisherId,
-      date: now,
-      email,
-      send,
-      ip,
-      ua,
-    });
+    // add ttl to the events collection of 24-48 hours?
+    return Promise.all([
+      // aggregated
+      aggregateEvent({
+        action: 'click',
+        ...ids,
+        adunitId,
+        deploymentId,
+        publisherId,
+        now,
+      }),
+
+      db.insertOne('events', {
+        ...ids,
+        type: 'click',
+        adunitId,
+        deploymentId,
+        publisherId,
+        date: now,
+        email,
+        send,
+        ip,
+        ua,
+      }),
+    ]);
   },
 
   request(adunit, {
@@ -71,16 +132,32 @@ module.exports = {
     ua,
   }) {
     const { _id: adunitId, deploymentId, publisherId } = adunit;
-    return db.insertOne('events', {
-      type: 'request',
-      adunitId,
-      deploymentId,
-      publisherId,
-      date: now,
-      email,
-      send,
-      ip,
-      ua,
-    });
+    return Promise.all([
+      // pre-aggregated
+      (async () => {
+        const month = dayjs.tz(now, 'America/Chicago').format('YYYY-MM');
+        const _id = { adunit: adunitId, month };
+        await db.collection('requests').updateOne({ _id }, {
+          $setOnInsert: { _id },
+          ...((deploymentId || publisherId) && {
+            $set: { deploymentId, publisherId },
+          }),
+          $inc: { n: 1 },
+        }, { upsert: true });
+      })(),
+
+      // single event
+      db.insertOne('events', {
+        type: 'request',
+        adunitId,
+        deploymentId,
+        publisherId,
+        date: now,
+        email,
+        send,
+        ip,
+        ua,
+      }),
+    ]);
   },
 };
